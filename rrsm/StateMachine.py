dbeaad48575e8f91e8c7ef4b7429193d89cab3bf
jayehnehm <jeffrey.mclarty@gmail.com>
2014-09-06 11:21:32 -0400

    Make the class more logical

--- rrsm/StateMachine.py
+++ rrsm/StateMachine.py
@@ -1,3 +1,5 @@
+
+
 class StateMachine(object):
     def __init__(self,RequiredStates,InitialState=0):
 
@@ -5,8 +7,8 @@ class StateMachine(object):
         if type(RequiredStates) is dict:
             assert all([type(state) is int for state in RequiredStates.values()]), "When RequiredStates is a dictionary, it's values must all be of type integer"
             assert len(RequiredStates.values()) == len(list(set(RequiredStates.values()))), "When RequiredStates is a dictionary, it's values must form a unique set"
-            self.States = RequiredStates
-            self.StateCodes = dict([(code,state) for state,code in RequiredStates.iteritems()]) # This is done for speed of the rest of the class
+            self.States = dict([(code,state) for state,code in RequiredStates.iteritems()]) # This is done for speed of the rest of the class
+            self.StateCodes = RequiredStates
         elif type(RequiredStates) is list:
             assert len(RequiredStates) == len(list(set(RequiredStates))), "RequiredStates must be list of unique strings"
             self.States = dict([(code,state) for code,state in enumerate(RequiredStates)])
@@ -14,8 +16,8 @@ class StateMachine(object):
 
         self.SwitchTo(InitialState)
         
-        for StateCodes,States in self.States.iteritems():
-            setattr(StateMachine,States,StateCodes)
+        for code,state in self.States.iteritems():
+            setattr(StateMachine,state,code)
     def __repr__(self):
         return "StateMachine(" + str(self.StateCodes) + ",InitialState=" + str(self.CurrentCode) + ")"
     def SwitchTo(self,NewState):
