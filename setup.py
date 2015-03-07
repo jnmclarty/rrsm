07d2acb52bdfb21f00827ea28ab6d962a7fd0506
jnmclarty <jeffrey.mclarty@gmail.com>
2015-03-06 21:22:45 -0500

    Fix setup, add license

--- setup.py
+++ setup.py
@@ -1,17 +1,27 @@
-from setuptools import setup, find_packages
+import os
 
-with open('README.rst') as file:
-    long_description = file.read(
-    
-setup(
-    name = "",
-    version = "0.1",
-    packages = find_packages(),
+from setuptools import setup
 
-    # metadata for upload to PyPI
-    author = "Jeffrey McLarty",
-    author_email = "jeffrey.mclarty@gmail.com",
-    description = "This is an Example Package",
-    license = "PSF",
-    keywords = "readable runtime run-time finite state machine"
-)
+def read(*paths):
+    """Build a file path from *paths* and return the contents."""
+    with open(os.path.join(*paths), 'r') as f:
+        return f.read()
+        
+v = '0.1.0'
+print "Setting up sympa" 
+  
+setup(name = "rrsm",
+      version = v,
+      packages = ['rrsm'],
+      description = 'Readable run-time state machine',
+      long_description=(read('README.rst')),    
+      author = "Jeffrey McLarty",
+      author_email = "jeffrey.mclarty@gmail.com",
+      url = 'https://github.com/jnmclarty/rrsm',
+      download_url = 'https://github.com/jnmclarty/rrsm/tarball/' + v,    
+      keywords = "readable runtime run-time finite state machine".split(" "),
+      classifiers = ['Development Status :: 1 - Planning',
+                     'License :: OSI Approved :: MIT License',
+                     'Intended Audience :: Developers',
+                     'Operating System :: OS Independent',
+                     'Programming Language :: Python :: 2.7'])
--- usage.py
+++
@@ -1,3 +0,0 @@
-from rrsm import StateMachine
-
-x = StateMachine({0:'A', 2:'B'})
